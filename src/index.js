cdd81d657c5a842cd2cdd997b8e500b89477468b
Kassandra <K1995Moreno@gmail.com>
2022-11-06 01:08:35 -0500

    commit tree cdbac1111dfd82371c9c6a6b78e98a0c77c94f6c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+
+
+const init = () => {
+    const inputForm= document.querySelector('form')
+    inputForm.addEventListener('submit',(e) => {
+        e.preventDefault()
+        const input = document.querySelector('input#searchByID')
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp =>resp.json()) 
+        .then(movies => {
+            console.log(movies)
+        })
+    })
+}
+document.addEventListener('DOMContentLoaded', init)
+
